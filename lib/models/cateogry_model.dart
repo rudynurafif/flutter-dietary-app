51072df41cff39f1bb80dbb00f736c7b8cd6490c
Rudy Nurafif <rudynurafif@gmail.com>
2024-04-16 18:24:31 +0700

    first commit

---
+++ lib/models/cateogry_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  late String name;
+  late String iconPath;
+  late Color boxColor;
+
+  CategoryModel({required this.name, required this.iconPath, required this.boxColor});
+
+  static List<CategoryModel> getCategories() {
+    List<CategoryModel> categories = [];
+
+    categories.add(CategoryModel(name: 'Salad', iconPath: 'assets/icons/plate.svg', boxColor: const Color(0xff92A3FD)));
+
+    categories
+        .add(CategoryModel(name: 'Cake', iconPath: 'assets/icons/pancakes.svg', boxColor: const Color(0xffc588F2)));
+
+    categories.add(CategoryModel(name: 'Pie', iconPath: 'assets/icons/pie.svg', boxColor: const Color(0xff92A3FD)));
+
+    categories
+        .add(CategoryModel(name: 'Smoothies', iconPath: 'assets/icons/orange-snacks.svg', boxColor: Color(0xffC588F2)));
+
+    return categories;
+  }
+}
